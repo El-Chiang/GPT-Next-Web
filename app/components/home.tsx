9e9a1d36bcb87130964d485d5cbe5fd1d9137e1e
不狸 <junyu.junyujiang@alibaba-inc.com>
2023-04-11 00:33:38 +0800

    feat: 文案调整

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -176,9 +176,9 @@ function _Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+          <div className={styles["sidebar-title"]}>ChatGPT 1984</div>
           <div className={styles["sidebar-sub-title"]}>
-            Build your own AI assistant.
+            Make Macintosh Great Again.
           </div>
           <div className={styles["sidebar-logo"]}>
             <ChatGptIcon />
--- app/layout.tsx
+++ app/layout.tsx
@@ -17,10 +17,10 @@ try {
 }
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "ChatGPT 1984",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "ChatGPT 1984",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
